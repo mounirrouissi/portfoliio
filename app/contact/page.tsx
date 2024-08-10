b0be38e676231b0b0535b184b3e2d36bb23a1b6e
Hamed Bahram <hamed.bahram@gmail.com>
2024-08-10 17:09:59 -0400

    Add contact page, resend package

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,12 @@
+import ContactForm from '@/components/contact-form'
+
 export default function Contact() {
   return (
     <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
-        <h1 className='title'>Contact</h1>
+        <h2 className='title'>Let&apos;s talk about your project</h2>
+
+        <ContactForm />
       </div>
     </section>
   )
---
+++ lib/actions.ts
@@ -0,0 +1,34 @@
+'use server'
+
+import { z } from 'zod'
+import { Resend } from 'resend'
+import { ContactFormSchema } from '@/lib/schemas'
+import ContactFormEmail from '@/emails/contact-form-email'
+
+type ContactFormInputs = z.infer<typeof ContactFormSchema>
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function sendEmail(data: ContactFormInputs) {
+  const result = ContactFormSchema.safeParse(data)
+
+  if (result.success) {
+    const { name, email, message } = result.data
+    try {
+      const data = await resend.emails.send({
+        from: 'hello@hamedbahram.io',
+        to: [email],
+        cc: ['hello@hamedbahram.io'],
+        subject: 'Contact form submission',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+        react: ContactFormEmail({ name, email, message })
+      })
+      return { success: true, data }
+    } catch (error) {
+      return { success: false, error }
+    }
+  }
+
+  if (result.error) {
+    return { success: false, error: result.error.format() }
+  }
+}
