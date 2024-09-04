cd9c028f8337030b903bb384392035fe245f9ebd
Monir11 <mounirrouissi2@gmail.com>
2024-09-04 13:05:50 +0200

    update ui

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -140,7 +140,7 @@ export default function ContactForm() {
               disabled={isSubmitting}
               className='w-full disabled:opacity-50'
             >
-              {isSubmitting ? 'Submitting...' : 'Contact Us'}
+              {isSubmitting ? 'Submitting...' : 'Contact Me'}
             </Button>
           </div>
           <p className='mt-4 text-xs text-muted-foreground'>
